8858c307893fb153c6db4c17cbfc9cf54ed7dbfc
barbvilla <barbara.villasenor.c@gmail.com>
2023-10-23 16:37:23 -0300

    esquema navbar

---
+++ itau-1/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+      <img alt='logo'src='itau.png'></img>
+      <p>Inicio de sesión</p>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
