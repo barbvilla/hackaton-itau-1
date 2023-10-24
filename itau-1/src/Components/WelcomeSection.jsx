8e18cf6963a3b9a5ef7f22e4956f78e0f7065f31
solsepulveda <solangesepulved@gmail.com>
2023-10-24 18:00:42 -0300

    pantalla de welcome

---
+++ itau-1/src/Components/WelcomeSection.jsx
@@ -0,0 +1,38 @@
+import './WelcomeSection.css';
+
+const WelcomeSection = () => {
+  return (
+    <>
+    <h1>¡Te damos la bienvenida a iBot!</h1>
+    <div class='btn-section'>
+    <img alt='botón chatbot' src='asistencia-btn.png' class='btn-chatbot'></img>
+    </div>
+    <section class='squares'>
+      <div class='square-1'>
+        <div class='titulo'>
+          <h3>Novedades iBot</h3>
+        </div>
+        <div class='info'>
+          <ul>
+            <li>Actualizaciones recientes. 20.10.2023</li>
+            <li>Actualizaciones recientes. 20.10.2023</li>
+            <li>Chatbot y sus beneficios</li>
+          </ul>
+        </div>
+      </div>
+      <div class='square-2'>
+      <div class='titulo'>
+          <h3>Preguntas frecuentes</h3>
+        </div>
+        <div class='info'>
+        <li>¿Cómo inicio una conversación?</li>
+            <li>Solicitar un requerimiento</li>
+            <li>Reportar un incidente</li>
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default WelcomeSection;
