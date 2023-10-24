7e8c741358b426bdb91b36be89c1484b3e178181
barbvilla <barbara.villasenor.c@gmail.com>
2023-10-24 15:23:18 -0300

    avance funcionalidad login firebase

--- itau-1/src/Components/LoginSection.jsx
+++ itau-1/src/Components/LoginSection.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
+import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import { auth } from '../firebase';
+import { NavLink, useNavigate } from 'react-router-dom'
 import './LoginSection.css'
 
-const LoginSection = () => {
+export const LoginSection = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate("/home")
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      });
+  }
 
   return (
-
     <section className="login">
       <div className="loginform">
         <h2>Iniciar sesión</h2>
-        <form action="submit">
+        <form>
           <label>Correo</label>
-          <input type="email" />
+          <input id="email "name="email" type="email" required placeholder="Email address" onChange={(e)=>setEmail(e.target.value)} />                           
           <label>Contraseña</label>
-          <input type="password" />
-          <input type="button" className='button-login' value='Ingresar'/>
+          <input id="password" name="password" type="password" required placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+          <input type="button" className='button-login' value='Ingresar' onClick={onLogin} />
         </form>
       </div>
       <div>
