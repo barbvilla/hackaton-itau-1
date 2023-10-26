690e442e2f61bc0416af790a9b0bea4f24d0800c
solsepulveda <solangesepulved@gmail.com>
2023-10-26 00:32:38 -0300

    mejorando welcome

--- itau-1/src/Components/WelcomeSection.jsx
+++ itau-1/src/Components/WelcomeSection.jsx
@@ -3,34 +3,42 @@ import './WelcomeSection.css';
 const WelcomeSection = () => {
   return (
     <>
-    <h1 class='bienvenida'>¡Te damos la bienvenida a iBot!</h1>
-    <div class='btn-section'>
-    <img alt='botón chatbot' src='asistencia-btn.png' class='btn-chatbot'></img>
-    </div>
-    <section class='squares'>
-      <div class='square-1'>
-        <div class='titulo'>
-          <h3>Novedades iBot</h3>
-        </div>
-        <div class='info'>
-          <ul>
-            <li>Actualizaciones recientes. 20.10.2023</li>
-            <li>Actualizaciones recientes. 20.10.2023</li>
-            <li>Chatbot y sus beneficios</li>
-          </ul>
-        </div>
+      <h1 class='bienvenida'>¡Te damos la bienvenida a iBot!</h1>
+      <div class='btn-section'>
+        <img alt='botón chatbot' src='asistencia-btn.png' class='btn-chatbot'></img>
       </div>
-      <div class='square-2'>
-      <div class='titulo'>
-          <h3>Preguntas frecuentes</h3>
+      <section class='squares'>
+        <div class='square-1'>
+          <div class='titulo'>
+            <h3 class='head'>Novedades iBot</h3>
+          </div>
+          <div class='info'>
+            <ul>
+              <li>Actualizaciones recientes. 20.10.2023</li>
+              <hr/>
+              <li>Actualizaciones recientes. 20.10.2023</li>
+              <hr/>
+              <li>Chatbot y sus beneficios</li>
+              <hr/>
+            </ul>
+          </div>
         </div>
-        <div class='info'>
-        <li>¿Cómo inicio una conversación?</li>
-            <li>Solicitar un requerimiento</li>
-            <li>Reportar un incidente</li>
+        <div class='square-2'>
+          <div class='titulo'>
+            <h3 class='head'>Preguntas frecuentes</h3>
+          </div>
+          <div class='info'>
+            <ul>
+              <li>¿Cómo inicio una conversación?</li>
+              <hr/>
+              <li>Solicitar un requerimiento</li>
+              <hr/>
+              <li>Reportar un incidente</li>
+              <hr/>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   )
 }
--- itau-1/src/Views/Login.jsx
+++ itau-1/src/Views/Login.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../Components/Navbar"
 import LoginSection from "../Components/LoginSection"
-import Modal from "../Components/Modal"
+/* import Modal from "../Components/Modal" */
 
 export default function Login() {
     return (
         <>
         <Navbar />
         <LoginSection />
-        <Modal />
+        {/* <Modal /> */}
         </>
     )
 }
