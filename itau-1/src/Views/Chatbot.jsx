e6e739e476f9608ae459fffcd77d96e9c4c03dbd
solsepulveda <solangesepulved@gmail.com>
2023-10-26 12:15:31 -0300

    login y welcome responsivas

--- itau-1/src/Views/Chatbot.jsx
+++ itau-1/src/Views/Chatbot.jsx
@@ -1,7 +1,13 @@
+/* import { FullPageChat } from "flowise-embed-react";
+
 export default function Chatbot() {
     return (
         <>
+         <FullPageChat
+      chatflowid="8d348133-962a-46a9-85c9-3dc75c272a94"
+      apiHost="https://itau-1-tf.onrender.com"
+    />
         <h1>Chatbot</h1>
         </>
     )
-}
+} */
