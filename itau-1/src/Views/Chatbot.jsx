be8a3fe5983203a482631ad50d8ea116ba655dbb
barbvilla <barbara.villasenor.c@gmail.com>
2023-10-27 10:29:27 -0300

    vista chatbot avanzada

--- itau-1/src/Views/Chatbot.jsx
+++ itau-1/src/Views/Chatbot.jsx
@@ -1,46 +1,24 @@
-import { BubbleChat } from 'flowise-embed-react'
+import Navbar from '../Components/Navbar';
+import ChatbotSection from '../Components/chatbotSection';
+import RobotIn from '../Components/Chat-inventory';
+import RobotReInc from '../Components/Chat-req-inc';
+import './chat.css'
+
 
 export default function ChatBot() {
   return (
-    <BubbleChat
-      chatflowid="cbc95607-d89e-43f0-85d2-abcdfb9ce586"
-      apiHost="https://itau-1-tf.onrender.com"
-      theme={{
-        button: {
-          backgroundColor: "#3B81F6",
-          right: 20,
-          bottom: 20,
-          size: "medium",
-          iconColor: "white",
-          customIconSrc: "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg",
-        },
-        chatWindow: {
-          welcomeMessage: "Hola!, ¿en que te puedo ayudar?",
-          backgroundColor: "#ffffff",
-          height: 700,
-          width: 400,
-          fontSize: 16,
-          poweredByTextColor: "#303235",
-          botMessage: {
-            backgroundColor: "#f7f8ff",
-            textColor: "#303235",
-            showAvatar: true,
-            avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/parroticon.png",
-          },
-          userMessage: {
-            backgroundColor: "#3B81F6",
-            textColor: "#ffffff",
-            showAvatar: true,
-            avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
-          },
-          textInput: {
-            placeholder: "Type your question",
-            backgroundColor: "#ffffff",
-            textColor: "#303235",
-            sendButtonColor: "#F25C05",
-          }
-        }
-      }}
-    />
+    <div className='chat'>
+      <Navbar />
+      <div className='chat-section'>
+        <div>
+          <ChatbotSection />
+        </div>
+        <div className='chatbox'>
+          <div className='chat-1'><RobotIn /></div>
+          <div className='chat-2'><RobotReInc /></div>
+          <div className='chat-3'><RobotIn /></div>
+        </div>
+      </div>
+    </div>
   );
 };
