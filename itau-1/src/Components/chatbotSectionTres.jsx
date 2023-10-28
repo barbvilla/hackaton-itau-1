c79fc08083c4dab125943ad5768c6cc9aade9447
solsepulveda <solangesepulved@gmail.com>
2023-10-27 23:50:32 -0300

    text-area

--- itau-1/src/Components/chatbotSectionTres.jsx
+++ itau-1/src/Components/chatbotSectionTres.jsx
@@ -1,23 +1,19 @@
 import './chatbotSection.css'
 
 
-const ChatbotSectionTres = () =>{
-  return(
+const ChatbotSectionTres = () => {
+  return (
     <>
-     <div class='section-info' id='vistapaso-3'>
+      <div class='section-info' id='vistapaso-3'>
         <div class='info'>
           <div class='empty'></div>
           <div class='paso-1'>
             <h1>Paso 3</h1>
             <p>Pega la información entregada por los bots aquí</p>
-            <textarea id="miTextArea" name="miTexto" placeholder="Escribe aquí..."></textarea>
+            <textarea id="textArea" name="miTexto" placeholder="Escribe aquí..."></textarea>
+            <button class='text-btn'>Generar ticket</button>
           </div>
           <div class='siguiente'>
-            <div>
-            {/* <p>Cuando obtengas tus datos</p> */}
-            <h3>Generar ticket</h3>
-            </div>
-        
           </div>
         </div>
       </div>
